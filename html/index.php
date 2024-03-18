1f9a1db72fbc42e69dd82ac3ee8dca5d0f43cf33
caiodohtmldocss <caiocesargaliti1@gmail.com>
2024-03-18 08:05:45 -0300

    paginas

--- html/index.php
+++ html/index.php
@@ -1,65 +1,38 @@
 <?php
+// Conexão com o banco de dados
+$servername = "localhost"; // endereço do servidor MySQL (geralmente localhost)
+$username = "root"; // nome de usuário do banco de dados
+$password = ""; // senha do banco de dados
+$dbname = "loginecoo"; // nome do banco de dados
+$table = "formulario_contato"; // nome da tabela
 
-if (isset($_POST['btnSend'])) {
+// Conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $msg = '';
-    
-    //campos do formulario
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $assunto = $_POST["assunto"];
-    $mensagem = $_POST['mensagem'];
-
-    //valida se os campos não estão vazios
-    if ((!empty($nome)) && (!empty($email)) && (!empty($telefone)) && (!empty($assunto)) && (!empty($mensagem))) {
-        
-        $email_remetente = "contato@cristianefaria.com"; // deve ser uma conta de email do seu dominio 
-        $email_destinatario = $email; // email que receberá as mensagens
-        $email_recebidoDe = "$email";
-        $email_assunto = "Envio Teste do Formulário de Contato | Cristiane Faria"; // Este será o assunto da mensagem
-        $email_conteudo = "FORMULÁRIO DE CONTATO\n"
-            . "<br><b>De:</b> " . $nome
-            . "<br><b>Email:</b> " . $email
-            . "<br><b>Telefone:</b> " . $telefone
-            . "<br><b>Assunto:</b> " . $assunto
-            . "<br><b>Mensagem:</b> " . $mensagem
-            . "<br><br>"
-            . "<hr>"
-            . "<br>Mensagem enviada do formulário de contato da demonstração de formulário de contato com php.";
-
-        //encapsula os dados do cabeçalho do email
-        $email_cabecalho = implode("\n", array("From: $email_remetente", "Reply-To: $email_recebidoDe", "Return-Path: $email_remetente", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
-        
-        //utiliza função nativa do php mail para o envio
-        //valida se o email foi enviado
-        if (mail($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_cabecalho)) {
-            
-            //mostra mensagem de envio com sucesso
-            $msg = '<div class="alert alert-success alert-dismissable">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                <strong>Mensagem enviada com sucesso!</strong> 
-            </div>';
-        } else {
-
-            //mostra mensagem de erro ao enviar
-            $msg = '<div class="alert alert-danger alert-dismissable">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                <strong>Erro ao enviar mensagem, tente novamente! </strong> 
-            </div>';
-        }
-        
-    } else {
-        
-        //mostra mensagem de erro caso algum dos campos esteja vazio
-        $msg = '<div class="alert alert-danger alert-dismissable">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                <strong>Preencha todos os campos!! </strong> 
-            </div>';
-    }
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-?>        
+// Recebe os dados do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
+
+// Prepara e executa a query SQL para inserir os dados no banco de dados
+$sql = "INSERT INTO $table (nome, email, telefone, assunto, mensagem)
+VALUES ('$nome', '$email', '$telefone', '$assunto', '$mensagem')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dados inseridos com sucesso!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>  
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -100,7 +73,9 @@ if (isset($_POST['btnSend'])) {
     </nav>
     </header>
 
-    
+    <br>
+    <br>
+    <br>
     <div id="section1">
         <div class="content">
             <h2 class="title">ECONNECT</h2>
@@ -258,7 +233,7 @@ if (isset($_POST['btnSend'])) {
         <div id="section4">
     <div id="container">
         <div class="faleconosco">
-            <form class="form-horizontal" action="index.php#formulario" method="post" role="form" data-toggle="validator">
+            <form class="form-horizontal" action="processa_formulario.php" method="post" role="form" data-toggle="validator">
                 <h1>Formulário de Contato</h1>
                 <div class="form-group">
                     <label class="control-label col-sm-3">Nome*:</label>
